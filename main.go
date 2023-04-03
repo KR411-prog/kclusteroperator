e6159aee2a712534dfba96c5acf6f82c792564f1
Radha Ramadoss <radharamadoss@radharamadoss.askey.com>
2023-04-03 13:58:40 -0700

    Initial files

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/KR411-prog/kclusteroperator/pkg/apis/radha/v1"
+	"log"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	k := v1.Kcluster{}
-	fmt.Println(k)
+	// k := v1.Kcluster{}
+	// fmt.Println(k)
+	// //https://github.com/viveksinghggits/ekspose/blob/master/main.go
+
+	// var kubeconfig *string
+	// kubeconfig = flag.String("kubeconfig", "/home/.kube/config", "location to your kubeconfig file")
+	// flag.Parse()
+	// config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	// if err != nil {
+	// 	// handle error
+	// 	if err != nil {
+	// 		log.Printf("Building config from flags %s", err.Error())
+	// 	}
+	// }
+
+	// klientset,err := klient.NewForConfig(config)
+	// if err != nil {
+	// 	log.Printf("")
+	// }
+	fmt.Println("test")
 }
--- pkg/apis/radha/v1/doc.go
+++ pkg/apis/radha/v1/doc.go
@@ -8,5 +8,7 @@
 // +k8s:defaulter-gen=TypeMeta
 // +groupName=radha
 
+//code-generator command not working for me
+// ./generate-groups.sh all github.com/KR411-prog/kcluster/pkg/client github.com/KR411-prog/kcluster/pkg/apis radha:v1 --go-header-file ./hack/boilerplate.go.txt  
 package v1
 
